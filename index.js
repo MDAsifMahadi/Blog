4ccf38609eacce78f43dafaa7999ccd8d7b0ba84
MDAsifMahadi <153858628+MDAsifMahadi@users.noreply.github.com>
2024-01-06 14:23:29 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// typing styles
+function typed(){
+    var typed = new Typed(".typing",{
+        strings: ["The Story Of Your Life","Write Your Story","Make a Life Style"],
+        typeSpeed: 80,
+        backSpeed: 80,
+        loop: true
+    });
+} 
+typed();
+
